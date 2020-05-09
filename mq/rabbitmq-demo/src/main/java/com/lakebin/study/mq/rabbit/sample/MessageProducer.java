27e00d82fe879cf49ceea89da5d5579e7bee3413
lakebin <fanghubin@cnpc.com.cn>
2020-05-09 17:46:26 +0800

    RabbitMQ Sample Demo

---
+++ mq/rabbitmq-demo/src/main/java/com/lakebin/study/mq/rabbit/sample/MessageProducer.java
@@ -0,0 +1,32 @@
+package com.lakebin.study.mq.rabbit.sample;
+
+import com.lakebin.study.mq.rabbit.util.RabbitConnectionUtil;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 消息生产者
+ */
+public class MessageProducer {
+    private static final Logger log = LoggerFactory.getLogger(MessageProducer.class);
+    private static final String QUEUE_NAME = "q_sample";
+
+    public static void main(String[] args) throws Exception {
+        //获取连接
+        Connection connection = RabbitConnectionUtil.getConnection();
+        //从连接中创建通道
+        Channel channel = connection.createChannel();
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME, false, true, true, null);
+        //消息体
+        String message = "Hello RabbitMQ";
+        //发送消息
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        log.debug("发送消息：{}", message);
+        //关闭通道和连接
+        channel.close();
+        connection.close();
+    }
+}
---
+++ mq/rabbitmq-demo/src/main/java/com/lakebin/study/mq/rabbit/util/RabbitConnectionUtil.java
@@ -0,0 +1,29 @@
+package com.lakebin.study.mq.rabbit.util;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * RabbitMq连接工具
+ */
+public class RabbitConnectionUtil {
+
+    /**
+     * 从连接工厂获取连接
+     * @return
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public static Connection getConnection() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.1.210");
+        factory.setVirtualHost("/lakebin");
+        factory.setUsername("admin");
+        factory.setPassword("123456");
+        //获取连接
+        return factory.newConnection();
+    }
+}
